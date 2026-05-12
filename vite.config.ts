6195da91cc3aee3fb304b6c39675857fd67fc35e
sutharprin098 <prince@bramb.du.ac.in>
2026-05-12 14:34:26 +0530

    chore: configure custom subdomain map3d.princesite.in

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/map3d/',
+  base: '/',
   plugins: [
     dts({
       insertTypesEntry: true,
